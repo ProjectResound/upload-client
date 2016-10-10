cad7396563b969a465f8a5777a8ffd8d10e2d47c
Louise Yang <louise.m.yang@gmail.com>
2016-10-10 14:46:59 -0700

    Got Flow library loaded.

--- src/app/hello.js
+++ src/app/hello.js
@@ -1,19 +1,24 @@
 import { Component } from '@angular/core';
 
-// let Flow;
+const Flow = require('@flowjs/flow.js/dist/flow.min');
 
 @Component({
   selector: 'fountain-app',
   template: require('./hello.html')
 })
+
 export class HelloComponent {
   constructor() {
-    // this.flow = new Flow({
-    //   target: '/api/photo/redeem-upload-token',
-    //   query: {upload_token: 'my_token'}
-    // });
-    this.hello = 'Hello World!';
+    this.hello = 'Hello Flow!';
 
-    // this.flow.assignBrowse(document.getElementById('browseButton'));
+    this.flow = new Flow({
+      target: '/api/photo/redeem-upload-token',
+      query: { upload_token: 'my_token' }
+    });
+
+    this.flow.on('fileAdded', (file, event) => {
+      console.log('file added');
+      console.log(file, event);
+    });
   }
 }
